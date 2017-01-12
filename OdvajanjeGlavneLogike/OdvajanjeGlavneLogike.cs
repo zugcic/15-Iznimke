951d00401740c130b35e460a6e7c0a3cd35ca5e2
Stjepan Zugcic <stzugcic@vsite.hr>
2017-01-12 20:42:35 +0100

    Napravljeno OdvajanjeGlavneLOgike

--- OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
+++ OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
@@ -6,16 +6,17 @@ namespace Vsite.CSharp
     {
         public static void IspisPovrh(int prvi, int zadnji)
         {
-            // TODO: Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
-            {
-                for (int n = prvi; n < zadnji; ++n)
-                {
-                    for (int k = 1; k <= n; ++k)
-                    {
-                        Console.WriteLine("{0} povrh {1} = {2}", n, k, Math.Povrh(n, k));
-                    }
-                }
-            }
+			// TODO: Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
+			try {
+				for (int n = prvi;n < zadnji;++n) {
+					for (int k = 1;k <= n;++k) {
+						Console.WriteLine("{0} povrh {1} = {2}", n, k, Math.Povrh(n, k));
+					}
+				}
+			} catch (Exception e) {	  // Sve iznnimke su izvedene iz Exception, hvata sve iznimke
+				Console.WriteLine(e);
+			}
+
         }
         static void Main(string[] args)
         {
